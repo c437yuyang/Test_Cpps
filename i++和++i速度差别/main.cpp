68dedfe3a4843381e52af39a05ea0fd4fea4e385
Peng <954222619@qq.com>
2017-04-05 08:41:26 +0800

    添加项目文件。

---
+++ i++和++i速度差别/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <time.h>
+#include <windows.h>
+using namespace std;
+int main(){
+
+	int nTestTime = 10000000000000;
+	DWORD timeStart = GetTickCount();
+	for (int i=0;i!= nTestTime;++i)
+	{
+		;
+	}
+	cout << "++i耗时:" << (GetTickCount() - timeStart) << endl;
+
+	timeStart = GetTickCount();
+	for (int i = 0; i != nTestTime;i++)
+	{
+		;
+	}
+	cout << "i++耗时:" << (GetTickCount() - timeStart) << endl;
+	system("pause");
+	//经过测试,日了狗一样的竟然i++更快！！！
+}
